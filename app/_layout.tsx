a0dbd6887d028f465a251a776b0404d10a76b623
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2024-06-21 12:17:53 +0300

    Add tab names

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,19 +4,21 @@ import { Drawer } from "expo-router/drawer";
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer
+      // backBehavior="history"
+      >
         <Drawer.Screen
           name="index" // This is the name of the page and must match the url from root
           options={{
             drawerLabel: "Home",
-            title: "overview",
+            title: "Home",
           }}
         />
         <Drawer.Screen
-          name="[id]" // This is the name of the page and must match the url from root
+          name="[id]/index" // This is the name of the page and must match the url from root
           options={{
-            drawerLabel: "User",
-            title: "overview",
+            drawerLabel: "Dynamic route",
+            title: "Dynamic route",
           }}
         />
       </Drawer>
